a25b373ae0783f1a159fe39a858037a3f5da93cb
Lili Molloy <liliana.molloy@gmail.com>
2018-12-15 11:27:17 +1100

    fixed CORS auth

--- api/api.js
+++ api/api.js
@@ -10,6 +10,11 @@ const port = process.env.PORT || 5000;
 const url = 'http://localhost:';
 
 app.use(express.json())//to access the body of the request
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get("/", (req, res) => {
     return res.send('Welcome to Rate My Dog! Get rating :D')
